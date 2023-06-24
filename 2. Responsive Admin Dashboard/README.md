d923086385f86020ea5b47b9e15b96d756ab16c6
Mercy <mercyomole229@gmail.com>
2023-06-24 15:24:00 +0100

    Pushing Responsive Admin Dashboard

---
+++ 2. Responsive Admin Dashboard/README.md
@@ -0,0 +1,11 @@
+Responsive Dashboard
+
+Built using:
+HTML
+CSS and 
+Vanilla Javascript
+
+It is responsive on various screens such as a Big Laptops || Tablets and Smaller Laptops || Small tablets and Mobile phones.
+It has the Light and Dark theme feature.
+
+I would be replicating it using React.js very soon.
---
+++ 2. Responsive Admin Dashboard/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Zeus Air Fryer',
+        productNumber: '20156',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Sony Gaming Console',
+        productNumber: '10118',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'HP Spectre X360 Laptop',
+        productNumber: '20187',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Mini Rechargeable Handfan',
+        productNumber: '67988',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Xbox Gaming Console',
+        productNumber: '11763',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'HP ELitebook Folio G3',
+        productNumber: '98876',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
